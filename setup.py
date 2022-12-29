5873c699179a0fcb3162c39f1f8d52c24c15e7f7
VincentGe <cosplox@outlook.com>
2022-12-29 23:25:29 +0800

    add built tools

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='NumericalToolkits',
+    version='0.0.1',
+    packages=['NumericalToolkits'],
+    url='https://github.com/CDU-Ge/NumericalToolkits',
+    license='MIT LICENSE',
+    author='Vin Ge',
+    author_email='cosplox@outlook.com',
+    description='Value-related toolkit'
+)
